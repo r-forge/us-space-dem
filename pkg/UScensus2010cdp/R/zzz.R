af167b43f0eb288bf4473379656b4cc9d4f3e805
zalmquist <zalmquist@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-07-17 20:02:12 +0000

    US Census 2010 current version
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/us-space-dem@8 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/UScensus2010cdp/R/zzz.R
@@ -0,0 +1,27 @@
+######################################################################
+#
+# zzz.R
+#
+# Written y Zack Almquist
+# Last Modified 2/26/09
+# Licensed under the GNU General Public License version 3 or later
+#
+# Part of the R/census package
+#
+# .onLoad is run when the package is loaded with library(UScensus2010cdp)
+#
+######################################################################
+
+.onAttach <- function(libname, pkgname){
+  	temp<-packageDescription("UScensus2010cdp")
+  	msg<-paste(temp$Package,": ",temp$Title,"\n",
+      "Version ",temp$Version,
+      " created on ",
+      temp$Date,".\n", sep="")
+  msg<-paste(msg,"copyright (c) 2014, Zack W. Almquist, University of Minnesota\n",sep="")
+
+  msg<-paste(msg,'For citation information, type citation("UScensus2010cdp").\n')
+  msg<-paste(msg,'Type help(package="UScensus2010cdp") to get started.\n')
+  packageStartupMessage(msg)
+}
+
