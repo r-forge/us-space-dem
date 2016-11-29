a1b550c4cc425144700bad649a72001dcdbe20fe
zalmquist (Dr. ) <almquist@umn.edu>
2016-11-29 16:56:18 +0000

    Updated poly.clipper and API
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/us-space-dem@55 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/UScensus2000/R/poly.clipper.R
+++ pkg/UScensus2000/R/poly.clipper.R
@@ -20,20 +20,24 @@ state <- check.state(state, statefips)
             utils::data(list = x, envir = parent.frame()) ###Check enviroment
             sp <- get(x)
         }
-
-int<-gIntersection(sp,city,byid=TRUE)
-int<-int@polyobj
+sp<-spTransform(sp,CRS(proj4string(int)))
+int<-spTransform(int,CRS(proj4string(sp)))
+int<-suppressWarnings(gIntersection(sp,city,byid = TRUE,drop_lower_td=TRUE))
+#int<-int@polyobj
 int<-as(int,"SpatialPolygonsDataFrame")
-int<-spChFIDs(int,sapply(strsplit(rownames(int@data)," "),"[[",1))
-m<-match(rownames(int@data),rownames(sp@data))
-int@data<-sp@data[m,]
-phat<-areaPoly(int)/areaPoly(sp[m,])
-phat[phat>1]<-1
-int@data<-int@data[,!sapply(int@data,is.character)]*as.vector(phat)
+#int<-spChFIDs(int,sapply(strsplit(rownames(int@data)," "),"[[",1))
+#int<-spChFIDs(int,sapply(strsplit(sapply(slot(int, "polygons"), function(i) slot(i, "ID"))," "),"[[",1))
+int<-spChFIDs(int,paste(rep(state,length(slot(int, "polygons"))),1:length(slot(int, "polygons")),sep="_"))
+data<-over(int,sp)
+int@data<-data
+
+#m<-match(rownames(int@data),rownames(sp@data))
+#int@data<-sp@data[m,]
+#phat<-areaPoly(int)/areaPoly(sp[m,])
+#phat[phat>1]<-1
+#int@data<-int@data[,!sapply(int@data,is.character)]*as.vector(phat)
 
 if (is.null(proj) == FALSE) {
-  #require(rgdal)
-  requireNamespace("rgdal")
   int <- spTransform(int, proj)
 }
 int
